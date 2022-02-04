967ce3b609a00b4c0437e7195893fd83cee0f6bf
makeshaart <makeshaart@gmail.com>
2022-02-04 14:50:05 +0300

    test task

---
+++ src/components/Floor/Floor.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Floor.css';
+
+function Floor() {
+  return (
+    <div className='floor' >
+    </div>
+  );
+}
+
+export default Floor;
---
+++ src/components/Table/Table.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Table.css';
+
+function Table() {
+  return (
+    <div >
+    </div>
+  );
+}
+
+export default Table;
