18dcc7e1212b4f6990b5cbc86f00ecdcf1e664a2
makeshaart <makeshaart@gmail.com>
2022-02-04 16:15:34 +0300

    test task

--- src/components/Floor/Floor.tsx
+++ src/components/Floor/Floor.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import Table from '../Table/Table';
 import './Floor.css';
 
 function Floor() {
   return (
     <div className='floor' >
+        <Table/>
+        <Table/>
+        <Table/>
+        <Table/>
+        <Table/>
+        <Table/>
+        <Table/>
+        <Table/>
+        <Table/>
+        <Table/>
     </div>
   );
 }
